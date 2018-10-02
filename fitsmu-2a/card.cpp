9b9aee297e828eb988e5beb625a9c7fe53e996c8
Jason Fitch <jasonpfitch@gmail.com>
2018-10-01 19:10:50 -0500

    added main() and fixed operator <<

--- fitsmu-2a/card.cpp
+++ fitsmu-2a/card.cpp
@@ -9,11 +9,11 @@
 
 #include "card.h"
 
-std::ostream operator << (std::ostream& os, card& c)
+std::ostream& operator << (std::ostream& os, card& c)
 // Overloaded output operator
 {
    return os << "Suit: " << c.getSuit() << "\nValue: " <<
-          std::to_string(c.getValue());
+          std::to_string(c.getValue()) << std::endl;
 }
 
 card::card(int value, std::string suit)
